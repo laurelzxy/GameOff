d9adb6af656cc9e1a4603c0b4b9234cf96744a4f
laurelzxy <53730508814@info.local>
2024-05-21 16:23:14 -0300

    Feat: Formulario

--- script.js
+++ script.js
@@ -17,3 +17,44 @@ const abrirFecharMenu = ()=>{
         menu.style.display = "inline-flex"
     }
 }
+
+const solicitarOrcamento = (event) => {
+    // Pegar valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    // Organizar objeto com os valores
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    // Enviar requisicao para a api
+    // 127.0.0.1 = localhost
+    // Método HTTP POST - Create -> Cadastrar ou criar 
+    fetch("http://localhost:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => {
+        console.log(resposta)
+
+        // Limpar os campos (inputs)
+        document.querySelector("#contato form").reset()
+
+        // Mostrar alert com msg de sucesso
+        alert("Solicitação cadastrada")
+    })
+    .catch(erro => {
+        // CASO ERRO - alert com msg erro
+        console.error(erro)
+        alert("Erro na requisição")
+    })
+
+    event.preventDefault()
+}
